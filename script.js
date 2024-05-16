5678069d9bab57b1b35e248bb4a8a2e75ae7fd62
Tendai Junior Chakawuya <chakawuyat1@gmail.com>
2024-05-16 11:28:06 +0200

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let word = document.querySelectorAll(".word")
-words.forEach(word => {
-    let letters = word.textContent.split("");
-    word.tectContent="";
+const word = document.querySelectorAll(".word")
+word.forEach(word => {
+    const letters = word.textContent.split("");
+    word.textContent="";
     letters.forEach((letter)=>{
-        let span = document.createElement("span"); 
+        const span = document.createElement("span"); 
         span.textContent = letter;
         span.className = "letter";
         word.append(span);
@@ -37,3 +37,23 @@ let changeText = ()=>{
 
 changeText();
 setInterval(changeText,3000)
+
+
+//circle skill------------------------------------
+
+const circles = document.querySelectorAll('circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dos");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0 ; i < dots ; i++){
+        points += ` <div class="points" style="--i:1${i};--rot:${rotate}deg"></div>`;
+
+        elem.innerHTML = points;
+    } 
+
+
+})
